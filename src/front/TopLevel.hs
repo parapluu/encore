5efb153bbf8e4a0f6be2e02ced676adbab6a6e04
Dave Clarke <dave.clarke@cs.kuleuven.be>
2014-04-14 17:14:31 +0200

    Toying with toy top level.

--- src/front/TopLevel.hs
+++ src/front/TopLevel.hs
@@ -3,8 +3,10 @@ module Main where
 
 import AST
 import PrettyPrinter
+-- import CodeGen
 
 main = do
          putStrLn "Encore .... Off course."
          putStrLn $ show $ ppProgram example
+         -- ccode = codeGenP example
   where example = AST.example
