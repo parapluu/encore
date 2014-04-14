6b0efd5ef2e1a42a9b02679777706df05f751f1c
Dave Clarke <dave.clarke@cs.kuleuven.be>
2014-04-14 11:27:11 +0200

    Added first version of AST

---
+++ src/ir/AST.hs
@@ -0,0 +1,31 @@
+
+type Name = String
+type Type = String
+
+data Program = [ClassDecl]
+
+data ClassDecl = Class Name [FieldDecl] [MethodDecl]
+
+data FieldDecl = Field Type Name
+
+data MethodDecl = Method Name [ArgDecl] Type Expr
+
+type ArgDecl = (Type, Name)
+
+data Expr = Skip
+          | Call Expr Name [Expr]
+          | Let Name Expr Expr
+          | IfThenElse Expr Expr Expr
+          | Get Expr
+          | FieldAccess Expr Name
+          | FieldAssign Expr Name Expr
+          | VarAccess Name
+          | VarAssign Name Expr
+          | Null
+          | New Name
+          | Print Expr
+          | StringLiteral String
+          | IntLiteral Int
+          | Binop Op Expr Expr
+
+data BinExp = LT | GT | EQ | NEQ
