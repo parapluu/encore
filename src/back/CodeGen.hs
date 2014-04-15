470165af0abbe2afc21e542994452733158140cf
Stephan Brandauer <stephan.brandauer@it.uu.se>
2014-04-15 11:42:27 +0200

    'make run' target for translating the testprogram

--- src/back/CodeGen.hs
+++ src/back/CodeGen.hs
@@ -13,30 +13,30 @@ class Translatable a b where
   translate :: a -> b
 
 instance Translatable Expr CCode where
-  translate (Skip) = undefined
-  translate (Call cr) = undefined
-  translate (Let name e_init e_body) = undefined
-  translate (Seq es) = undefined
-  translate (IfThenElse e1 e2 e3) = undefined
-  translate (Get e) = undefined
-  translate (FieldAccess e name) = undefined
-  translate (Assign lvar e) = undefined
-  translate (VarAccess name) = undefined
-  translate (Null) = undefined
-  translate (New name) = undefined
-  translate (Print e) = undefined
-  translate (StringLiteral s) = undefined
+  translate (Skip) = (Embed "undefined")
+  translate (Call cr) = (Embed "undefined")
+  translate (Let name e_init e_body) = (Embed "undefined")
+  translate (Seq es) = (Embed "undefined")
+  translate (IfThenElse e1 e2 e3) = (Embed "undefined")
+  translate (Get e) = (Embed "undefined")
+  translate (FieldAccess e name) = (Embed "undefined")
+  translate (Assign lvar e) = (Embed "undefined")
+  translate (VarAccess name) = (Embed "undefined")
+  translate (Null) = (Embed "undefined")
+  translate (New name) = (Embed "undefined")
+  translate (Print e) = (Embed "undefined")
+  translate (StringLiteral s) = (Embed "undefined")
   translate (IntLiteral i) = (Embed $ show i)
-  translate (Binop op e1 e2) = undefined
+  translate (Binop op e1 e2) = (Embed "undefined")
 
 instance Translatable ParamDecl CCode where
-  translate = undefined
+  translate = (Embed "undefined")
 
 instance Translatable MethodDecl CCode where
-  translate = undefined
+  translate = (Embed "undefined")
 
 instance Translatable FieldDecl CCode where
-  translate = undefined
+  translate = (Embed "undefined")
 
 instance Translatable ClassDecl CCode where
   translate cdecl = C $
