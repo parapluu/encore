af47315cab0dec4a0e631e36d6b31023c5b3734a
Dave Clarke <dave.clarke@cs.kuleuven.be>
2014-04-17 12:45:05 +0200

    Added rudimentary printf support

--- src/back/CodeGen.hs
+++ src/back/CodeGen.hs
@@ -14,21 +14,22 @@ class Translatable a b where
   translate :: a -> b
 
 instance Translatable Expr CCode where
-  translate (Skip) = (Embed "")
-  translate (Call target name args) = (Embed "//whatever a Call does")
-  translate (Let name e_init e_body) = (Embed "//whatever a Let does")
-  translate (Seq es) = (Embed "//whatever a Seq does")
-  translate (IfThenElse e1 e2 e3) = (Embed "//whatever a IfThenElse does")
-  translate (Get e) = (Embed "//whatever a Get does")
-  translate (FieldAccess e name) = (Embed "//whatever a FieldAccess does")
-  translate (Assign lvar e) = (Embed "//whatever a Assign does")
-  translate (VarAccess name) = (Embed "//whatever a VarAccess does")
-  translate (Null) = (Embed "//whatever a Null does")
-  translate (New name) = (Embed "//whatever a New does")
-  translate (Print e) = (Embed "//whatever a Print does")
-  translate (StringLiteral s) = (Embed "//whatever a StringLiteral does")
-  translate (IntLiteral i) = (Embed $ show i)
-  translate (Binop op e1 e2) = (Embed "//whatever a Binop does")
+  translate (Skip) = Embed ""
+  translate (Call target name args) = Embed "//whatever a Call does"
+  translate (Let name e_init e_body) = Embed "//whatever a Let does"
+  translate (Seq es) = Embed "//whatever a Seq does"
+  translate (IfThenElse e1 e2 e3) = Embed "//whatever a IfThenElse does"
+  translate (Get e) = Embed "//whatever a Get does"
+  translate (FieldAccess e name) = Embed "//whatever a FieldAccess does"
+  translate (Assign lvar e) = Embed "//whatever a Assign does"
+  translate (VarAccess name) = Embed "//whatever a VarAccess does"
+  translate (Null) = Embed "//whatever a Null does"
+  translate (New name) = Embed "//whatever a New does"
+  translate (Print (StringLiteral s)) = Embed $ "printf(\"%s\"," ++ s ++ " );"
+  translate (Print e) = Embed "//whatever a Print does"
+  translate (StringLiteral s) = Embed "//whatever a StringLiteral does"
+  translate (IntLiteral i) = Embed $ show i
+  translate (Binop op e1 e2) = Embed "//whatever a Binop does"
 
 instance Translatable ParamDecl CCode where
   translate _ = (Embed "//whatever a ParamDecl does")
