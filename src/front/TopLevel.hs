845d9ee3aa3cba2cbc8a2266e4b554365b60d08b
EliasC <elias.castegren@gmail.com>
2014-04-17 16:22:28 +0200

    Minor clean-up

--- src/front/TopLevel.hs
+++ src/front/TopLevel.hs
@@ -11,25 +11,21 @@ import PrettyCCode
 import CodeGen
 
 main = do
-         putStrLn "//Encore .... Off course."
+         putStrLn "// Encore .... Off course."
          args <- getArgs
-         if null args then
+         if null args then 
              putStrLn usage
          else
              do 
                program <- return (lookup (head args) examples)
                case program of 
-                 Just ast -> 
-                     do 
-                       printCommented $ show $ ppProgram ast
-                       putStrLn "//##############"
-                       print $ code_from_AST ast
-                       -- ccode = codeGenP example             
-                 Nothing -> 
-                     do
-                       putStrLn "This is not a program that I can compile :("  
-                       putStrLn "Available programs are:"  
-                       mapM_ putStrLn progNames
+                 Just ast -> do printCommented $ show $ ppProgram ast
+                                printCommented "#####################"
+                                print $ code_from_AST ast
+                                -- ccode = codeGenP example             
+                 Nothing -> do putStrLn "This is not a program that I can compile :("  
+                               putStrLn "Available programs are:"  
+                               mapM_ putStrLn progNames
     where
       usage = "Usage: ./encorc [program-name]"
       printCommented s = putStrLn $ unlines $ map ("//"++) $ lines s
