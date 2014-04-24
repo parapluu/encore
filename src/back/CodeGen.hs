098013b2f10e87354bdf97eed12c8953cedad001
Stephan Brandauer <stephan.brandauer@it.uu.se>
2014-04-24 14:16:26 +0200

    refactor to use Reader monad for carrying context

--- src/back/CodeGen.hs
+++ src/back/CodeGen.hs
@@ -4,7 +4,10 @@
 module CodeGen (code_from_AST) where
 import Data.List
 import Data.Char
+import Data.Maybe
 import CCode
+import Control.Monad.Reader
+
 import qualified AST as A
 
 code_from_AST :: A.Program -> CCode
@@ -13,9 +16,16 @@ code_from_AST = translate
 class Translatable a b where
   translate :: a -> b
 
-instance Translatable A.Lvar CCode where
-  translate (A.LVar name) = Embed name
-  translate (A.LField ex name) = (Deref $ translate ex) `Dot` name
+data Context = Context { ctxt_prog :: A.Program,
+                         ctxt_class :: Maybe A.ClassDecl}
+
+ctxt_other_classes :: Context -> [A.ClassDecl]
+ctxt_other_classes ctxt =
+  let
+    (A.Program the_classes) = (ctxt_prog ctxt)
+    the_class = (ctxt_class ctxt)
+  in
+   map fromJust $ filter (/= the_class) (map Just the_classes)
 
 instance Translatable A.Op CCode where
   translate A.LT = Embed "<"
@@ -25,35 +35,72 @@ instance Translatable A.Op CCode where
   translate A.PLUS = Embed "+"
   translate A.MINUS = Embed "-"
 
-instance Translatable A.Expr CCode where  
-  translate (A.Skip)                      = Embed "/* skip */"
-  translate (A.Null)                      = Embed "0"
-  translate (A.Binop op e1 e2)            = C [(Embed "("),
-                                               translate e1,
-                                               translate op,
-                                               translate e2,
-                                               (Embed ")")]
-  translate (A.Print (A.StringLiteral s)) = Embed $ "printf(\"%s\\n\", \"" ++ s ++ "\" );"
-  translate (A.Print (A.FieldAccess (A.VarAccess var) name)) = Embed $ "printf(\"%i\\n\", " ++ var ++ "->" ++ name ++ " );"
-  translate (A.Seq es)                    = C $ map (Statement . translate) es
-  translate (A.Assign lvar expr)          = Assign (translate lvar) (translate expr)
-  translate (A.VarAccess name)            = Embed name
-  translate (A.FieldAccess exp name)      = Deref (translate exp) `Dot` name
-  translate (A.IntLiteral i)              = Embed $ show i
-  translate (A.StringLiteral s)           = Embed $ show s
-  translate other = Embed $ "/* missing: " ++ show other ++ "*/"
+instance Translatable A.Lvar (Reader Context CCode) where
+  translate (A.LVar name) = return $ Embed name
+  translate (A.LField ex name) = do
+    tex <- translate ex
+    return $ (Deref tex) `Dot` name
 
-convertType :: String -> CType
-convertType ty = case ty of
+instance Translatable A.Expr (Reader Context CCode) where
+  translate (A.Skip)                      = return $ Embed "/* skip */"
+  translate (A.Null)                      = return $ Embed "0"
+  translate (A.Binop op e1 e2)            = do
+    te1 <- translate e1
+    te2 <- translate e2
+    return $ C [(Embed "("),
+                te1,
+                translate op,
+                te2,
+                (Embed ")")]
+  translate (A.Print (A.StringLiteral s)) = return $ Embed $ "printf(\"%s\\n\", \"" ++ s ++ "\" );"
+  translate (A.Print (A.FieldAccess (A.VarAccess var) name)) = return $ Embed $ "printf(\"%i\\n\", " ++ var ++ "->" ++ name ++ " );"
+  translate (A.Seq es)                    = do
+    tes <- mapM translate es
+    return $ C (map Statement tes)
+  translate (A.Assign lvar expr)          = do
+    texpr <- translate expr
+    tlvar <- translate lvar
+    return $ Assign tlvar texpr
+  translate (A.VarAccess name)            = return $ Embed name
+  translate (A.FieldAccess exp name)      = do
+    texp <- translate exp
+    return $ Deref texp `Dot` name
+  translate (A.IntLiteral i)              = return $ Embed $ show i
+  translate (A.StringLiteral s)           = return $ Embed $ show s
+  translate (A.Let name ty e1 e2)         = do
+    te1 <- translate e1
+    te2 <- translate e2
+    return (BracedBlock $ C $
+            map Statement
+            ((Decl (CVarSpec (embedCType "pony_actor_t*", name))) :
+             [Assign (Var name) te1,
+              te2
+             ]))
+  translate (A.New ty) = return $ Embed $ "pony_create(&"++ty++"_actor_t)" -- continue here
+--  translate (A.Call { target=expr, tmname=name, args=as } ) = case expr of
+--    (VarAccess "this") -> Call 
+  translate other = return $ Embed $ "/* missing: " ++ show other ++ "*/"
+
+
+
+dataStructType :: String -> CType
+dataStructType ty = case ty of
   "Object" -> embedCType "void*"
   other_ty -> embedCType $ if isLower $ head other_ty
                            then other_ty
                            else other_ty++"_data*"
+
+pony_actor_t_Type :: String -> CType
+pony_actor_t_Type ty = case ty of
+  other_ty -> embedCType $ if isLower $ head other_ty
+                           then other_ty
+                           else other_ty++"_actor_t*"
+
 --instance Translatable [FieldDecl] CCode where
 --  translate fs = (StructDecl $ map VarDecl $ zip (map ftype fs) (map fname fs))
 
-instance Translatable A.ClassDecl CCode where
-  translate cdecl = C $
+instance Translatable A.ClassDecl (Reader Context CCode) where
+  translate cdecl = return $ C $
                     (comment_section $ "Implementation of class " ++ (A.cname cdecl)) :
                     data_struct :
                     tracefun_decl :
@@ -65,10 +112,10 @@ instance Translatable A.ClassDecl CCode where
       data_struct = Statement $
                     TypeDef data_struct_name
                     (StructDecl data_struct_name $ map CVarSpec $
-                     zip (map (convertType . A.ftype) (A.fields cdecl)) (map A.fname (A.fields cdecl)))
+                     zip (map (dataStructType . A.ftype) (A.fields cdecl)) (map A.fname (A.fields cdecl)))
 
 
-      dispatchfun_decl = (Function (convertType "static void") (A.cname cdecl ++ "_dispatch")
+      dispatchfun_decl = (Function (dataStructType "static void") (A.cname cdecl ++ "_dispatch")
                           (map CVarSpec [(embedCType "pony_actor_t*", "this"),
                                          (embedCType "void*", "p"),
                                          (embedCType "uint64_t", "id"),
@@ -116,10 +163,12 @@ instance Translatable A.ClassDecl CCode where
       dispatch_fn_name     = (A.cname cdecl) ++ "_dispatch"
 
       method_impl :: A.Type -> A.MethodDecl -> CCode
-      method_impl this_ty mdecl = (Function (convertType (A.rtype mdecl)) (A.cname cdecl ++ "_" ++ A.mname mdecl)
-                                   (CVarSpec (convertType this_ty, "this"):(map mparam_to_cvardecl $ A.mparams mdecl))
-                                   [translate (A.mbody mdecl)])
-      mparam_to_cvardecl (A.Param (ty, na)) = CVarSpec (convertType ty, na)
+      method_impl this_ty mdecl = (Function (dataStructType (A.rtype mdecl)) (A.cname cdecl ++ "_" ++ A.mname mdecl)
+                                   (CVarSpec (dataStructType this_ty, "this"):(map mparam_to_cvardecl $ A.mparams mdecl))
+                                   [Statement $ C[]
+                                    --runReader (translate (A.mbody mdecl)) (ask { ctxt_class = Just cdecl })
+                                   ])
+      mparam_to_cvardecl (A.Param (ty, na)) = CVarSpec (dataStructType ty, na)
 
 comment_section :: String -> CCode
 comment_section s = C $ [Embed $ take (5 + length s) $ repeat '/',
@@ -142,7 +191,7 @@ instance Translatable A.Program CCode where
                "inttypes.h",
                "assert.h"]) :
     (fwd_decls cs) ++
-    (map translate cs) ++
+    (map (translate_class) cs) ++
     [(Function
       (embedCType "static void") "dispatch"
       [CVarSpec (embedCType "pony_actor_t*", "this"),
@@ -167,6 +216,7 @@ instance Translatable A.Program CCode where
           fwd_decl cdecl = C $ [Embed $ "struct " ++ (A.cname cdecl) ++ "_data;",
                                 Embed $ "//todo fwd decl Main_message_type!!",
                                 Embed $ "static void " ++ (A.cname cdecl) ++ "_dispatch(pony_actor_t*, void*, uint64_t, int, pony_arg_t*);"]
-
-
-
+      the_context = Context { ctxt_prog = (A.Program cs),
+                              ctxt_class = Nothing }
+      translate_class :: A.ClassDecl -> CCode
+      translate_class cdecl = runReader (translate cdecl) the_context
