18fb51fc460046ceb3f4674f37779a0eee0891e7
EliasC <elias.castegren@gmail.com>
2014-05-16 15:17:09 +0200

    Included a prettyPrinter for EASTs

--- src/ir/EAST/PrettyPrinter.hs
+++ src/ir/EAST/PrettyPrinter.hs
@@ -1,1 +1,9 @@
-module PrettyPrinter where
+module EAST.PrettyPrinter where
+
+import Text.PrettyPrint
+
+import qualified AST.PrettyPrinter as PP
+import qualified EAST.EAST as EAST
+
+ppProgram :: EAST.Program -> Doc
+ppProgram = PP.ppProgram . EAST.toAST
