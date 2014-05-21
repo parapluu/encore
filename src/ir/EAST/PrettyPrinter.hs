0457e2250a3417a2b4f48fb37b79827291f2318c
Elias Castegren <elias.castegren@gmail.com>
2014-05-21 07:17:14 +0200

    Deleted the EAST

--- src/ir/EAST/PrettyPrinter.hs
+++
@@ -1,9 +0,0 @@
-module EAST.PrettyPrinter where
-
-import Text.PrettyPrint
-
-import qualified AST.PrettyPrinter as PP
-import qualified EAST.EAST as EAST
-
-ppProgram :: EAST.Program -> Doc
-ppProgram = PP.ppProgram . EAST.toAST
