28ff130a30bbb98fc24e284a89c0325245b548f0
Stephan Brandauer <stephan.brandauer@it.uu.se>
2014-05-25 12:49:24 +0200

    add name substitutions to context

--- src/back/CodeGen/Context.hs
+++ src/back/CodeGen/Context.hs
@@ -25,46 +25,52 @@ import Identifiers
 import Data.Maybe
 import Control.Monad.State
 
+import qualified CCode.Main as C
+
 type NextSym = Int
 
 type ProgramLoc = (Program, Maybe (ClassDecl, Maybe (MethodDecl, [ParamDecl])))
 
-data Context = Context ProgramLoc NextSym deriving (Show)
+type VarSubTable = [(Name, C.CCode C.Name)] -- variable substitutions (for supporting, for instance, nested var decls)
+
+data Context = Context VarSubTable ProgramLoc NextSym
 
 mk :: Program -> Context
-mk p = Context (p, Nothing) 0
+mk p = Context [] (p, Nothing) 0
 
 with_class :: ClassDecl -> Context -> Context
-with_class c (Context (p, Nothing) n) = Context (p, (Just (c, Nothing))) n
-with_class c (Context (p, (Just (_, m))) n) = Context (p, (Just (c, m))) n
+with_class c (Context s (p, Nothing) n)       = Context s (p, (Just (c, Nothing))) n
+with_class c (Context s (p, (Just (_, m))) n) = Context s (p, (Just (c, m))) n
 
 with_method :: MethodDecl -> Context -> Context
-with_method m (Context (p, (Just (c, _))) n) = Context (p, (Just (c, Just (m,[])))) n
+with_method m (Context s (p, (Just (c, _))) n) = Context s (p, (Just (c, Just (m,[])))) n
 
 gen_sym :: State Context String
 gen_sym = do
   c <- get
   case c of
-    Context lo n ->
+    Context s lo n ->
         do
-          put $ Context lo (n+1)
+          put $ Context s lo (n+1)
           return $ "_tmp" ++ show n
   
 with_local :: ParamDecl -> Context -> Context
-with_local d (Context (p, (Just (c, Just (m, ds)))) n) = Context (p, (Just (c, Just (m, d:ds)))) n
-with_local d c = error $ "with_local: invalid input: " ++ show (d, c)
+with_local d (Context s (p, (Just (c, Just (m, ds)))) n) =
+    Context s (p, (Just (c, Just (m, d:ds)))) n
+with_local d _ =
+    error $ "with_local: invalid input" ++ show d
 
 the_prog :: Context -> Program
-the_prog (Context (prog, _) _) = prog
+the_prog (Context _ (prog, _) _) = prog
 
 the_class :: Context -> Maybe ClassDecl
-the_class (Context (_, x) _) = x >>= Just . fst
+the_class (Context _ (_, x) _) = x >>= Just . fst
 
 the_method :: Context -> Maybe MethodDecl
-the_method (Context (_, x) _) = x >>= snd >>= return . fst
+the_method (Context _ (_, x) _) = x >>= snd >>= return . fst
 
 the_locals :: Context -> Maybe [ParamDecl]
-the_locals (Context (_, x) _) = x >>= snd >>= return . snd
+the_locals (Context _ (_, x) _) = x >>= snd >>= return . snd
 
 type_of :: Name -> Context -> Maybe Type
 type_of n c = do
