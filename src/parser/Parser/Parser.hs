43df4ee0ddf65559116f60f70e31d29ee0b26ff4
Stephan Brandauer <stephan.brandauer@it.uu.se>
2014-06-04 15:50:01 +0200

    remove skip as reserved word

--- src/parser/Parser/Parser.hs
+++ src/parser/Parser/Parser.hs
@@ -15,8 +15,7 @@ MethodDecls ::= def Name ( ParamDecls ) : Type Expr
        Args ::= , Expr Args | eps
        Path ::= Name FieldAccess | eps
 FieldAccess ::= . Name FieldAccess | eps
-       Expr ::= skip
-              | Path = Expr
+       Expr ::= Path = Expr
               | Path . Name ( Arguments )
               | let Name = Expr in Expr
               | { Sequence }
@@ -86,7 +85,7 @@ lexer =
                P.commentEnd = "-}",
                P.commentLine = "--",
                P.identStart = letter,
-               P.reservedNames = ["passive", "class", "def", "skip", "let", "in", "if", "then", "else", "while", "get", "null", "true", "false", "new", "print", "embed", "end", "Fut", "Par"],
+               P.reservedNames = ["passive", "class", "def", "let", "in", "if", "then", "else", "while", "get", "null", "true", "false", "new", "print", "embed", "end", "Fut", "Par"],
                P.reservedOpNames = [":", "=", "==", "!=", "<", ">", "+", "-", "*", "/", "->", "\\", "()"]
              }
 
