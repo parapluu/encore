a7adb180135285dbf596f2248a812f6e3f4d265c
Stephan Brandauer <stephan.brandauer@it.uu.se>
2014-06-06 11:59:18 +0200

    remove preprocessor

--- src/back/CodeGen/Main.hs
+++ src/back/CodeGen/Main.hs
@@ -7,7 +7,7 @@ Translating an Encore program to its CCode representation (see "CCode.Main")
 module CodeGen.Main (code_from_AST) where
 
 import CodeGen.Program
-import CodeGen.Preprocessor
+--import CodeGen.Preprocessor
 import CCode.Main
 
 import qualified AST.AST as A
@@ -15,4 +15,4 @@ import qualified AST.Util as Util
 
 -- | Translates a full Encore Program to a CCode FIN (see "CCode.Main") value
 code_from_AST :: A.Program -> CCode FIN
-code_from_AST = translate . preprocess
+code_from_AST = translate
