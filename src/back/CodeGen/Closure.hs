1f82c02416253d4798604e413d15c3eb5f7cdabb
Elias Castegren <elias.castegren@gmail.com>
2014-06-11 09:58:01 +0200

    Closures can now capture variables
    There are no restrictions on what can be captured, so no safety is
    guaranteed. The implementation currently uses malloc to set the
    environment up (so that it can be freed). Maybe this should be done by
    pony_alloc instead some day (as well as the allocation of the closure).
    
    Capturing variables turned out to give some kind of not-so-flexible currying:
    
    class Main
      def main() : void
        let compose = \(g : int -> int, f : int -> int) -> \(x : int) -> g(f(x)) in
          let double = \(x : int) -> x*2 in
            let bump = \(x : int) -> x+1 in
              let f = compose(double, bump) in
                print f(3)

--- src/back/CodeGen/Closure.hs
+++ src/back/CodeGen/Closure.hs
@@ -13,25 +13,32 @@ import qualified AST.AST as A
 import qualified AST.Util as Util
 
 import Types as Ty
+import Identifiers as Id
 
 import Control.Monad.Reader hiding(void)
 import Control.Monad.State hiding(void)
+import Data.Maybe
 
 translateClosure :: A.Expr -> (Reader Ctx.Context (CCode Toplevel))
 translateClosure closure 
     | A.isClosure closure = 
-        do let arrowType = A.getType closure
+        do ctx <- ask
+           let arrowType = A.getType closure
                resultType = Ty.getResultType arrowType
                argTypes = Ty.getArgTypes arrowType
                params = A.eparams closure
                body = A.body closure
                id = A.getMetaId closure
-           ctx <- ask
+               freeVars = Util.freeVariables (map A.pname params) body
            let ((bodyName, bodyStat), _) = runState (translate body) ctx
-           return $ Function (voidOrValue resultType) (closure_fun_name id)
-                      [(Typ "value", Var "args[]"), (Ptr void, Var "env")]
-                      (Seq $ (extractArguments params) ++ 
-                       [bodyStat, returnStmnt bodyName resultType])
+           return $ ConcatTL 
+                      [buildEnvironment (closure_env_name id) freeVars,
+                       Function (voidOrValue resultType) (closure_fun_name id)
+                         [(Typ "value", Var "_args[]"), (Ptr void, Var "_env")]
+                         (Seq $ 
+                            extractArguments params ++ 
+                            extractEnvironment (closure_env_name id) freeVars ++
+                            [bodyStat, returnStmnt bodyName resultType])]
     | otherwise = error "Tried to translate a closure from something that was not a closure"
     where
       voidOrValue ty
@@ -53,8 +60,20 @@ translateClosure closure
           where
             ty = translate ptype
             arg = Var $ show pname
-            getArgument i = Call getValFun [ArrAcc i (Var "args")]
+            getArgument i = Call getValFun [ArrAcc i (Var "_args")]
             getValFun
                 | isIntType ptype  = Nam "val_to_int"
                 | isRealType ptype = Nam "val_to_dbl"
                 | otherwise        = Nam "val_to_ptr"
+
+      buildEnvironment name members = 
+          StructDecl (Typ $ show name) (map translate_binding members)
+              where
+                translate_binding (name, ty) = (translate ty, Var $ show name)
+
+      extractEnvironment _ [] = []
+      extractEnvironment envName ((name, ty):vars) = 
+          (Assign (Decl (translate ty, Var $ show name)) (getVar name)) : extractEnvironment envName vars
+              where
+                getVar name = 
+                    (Deref $ Var $ "((struct ___" ++ (show envName) ++ "*) " ++ "_env)") `Dot` (Nam $ show name)
