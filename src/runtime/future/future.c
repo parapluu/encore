3a16a98663b52610fc5e3fa562a41d77fc7b0664
Tobias Wrigstad <tobias@jeepen.org>
2014-06-16 01:56:12 +0200

    New future implementation using the eager tit library. Refactored future actor to remove dependencies on changes to the Pony runtime. (Some changes to Pony runtime will always be needed for futures.)

--- src/runtime/future/future.c
+++ src/runtime/future/future.c
@@ -7,28 +7,19 @@
 #include <pthread.h>
 #include <ucontext.h>
 #include "future.h"
-#include "ccontext.h"
 #include "future_actor.h"
+#include "actor_def.h"
 
-extern pony_actor_type_t future_actor_type;
+struct future {
+  pony_actor_t actor;
+};
 
-// This function is almost verbatim actor_create from actor.c, but
-// with the important difference of reserving space for two fields
-// at the end holding the future state. 
-pony_actor_t* future_create() {
-  int INDEX = sizeof(future_payload) + sizeof(future_actor_type);
-  future *fut = (future*) pool_alloc(&pool_thread[INDEX], &pool_global[INDEX]);
-  return actor_create_stage_two(&future_actor_type, &(fut->actor));
-}
-  
-future *createNewFuture() {
-  future *fut = (future*) future_create();
-  fut->payload.fulfilled = false;
-  fut->payload.value = NULL;
-  return fut; 
+future_t *create_new_future() {
+  future_t *fut = (future_t*) future_create();
+  return fut;
 }
 
-void chain(future *f, pony_actor_t* a, struct closure *c) {
+void chain(future_t *f, pony_actor_t* a, struct closure *c) {
   pony_arg_t argv[2];
   argv[0].p = a;
   argv[1].p = c;
@@ -36,64 +27,66 @@ void chain(future *f, pony_actor_t* a, struct closure *c) {
   pony_sendv(&f->actor, FUT_MSG_CHAIN, 2, argv);
 }
 
-void block(future *f, pony_actor_t* a) {
-  stacklet_t *s = t_init();
+void block(future_t *f, pony_actor_t* a, void *s) {
+  if (!s) s = t_init();
 
-  pony_arg_t argv[2];
+  pony_arg_t argv[1];
   argv[0].p = a;
   argv[1].p = s;
   fprintf(stderr, "[%p]\t%p <--- block (%p) from %p \n", pthread_self(), f, s, a);
+
+  // XXX: When self-send below is in place, no need to send the stacklet to the future actor
   pony_sendv(&f->actor, FUT_MSG_BLOCK, 2, argv);
 
-  t_restart(s, a);
+  // XXX: Waiting on Sylvan's implementation of this
+  // pony_block_actor(a);
+  // XXX: Waiting on Sylvan's implementation of this
+  // pony_sendp_FRONT_OF_QUE(a, FUT_MSG_RESUME, s);
+
+  t_suspend((tit_t*) s);
 }
 
-extern void pony_actor_unblock(pony_actor_t *a);
-
-void await(future *f, pony_actor_t* a) {
-  stacklet_t *s = t_init();
+void await(future_t *f, pony_actor_t* a) {
+  tit_t *s = t_init();
 
   pony_arg_t argv[2];
   argv[0].p = a;
   argv[1].p = s;
   fprintf(stderr, "[%p]\t%p <--- block (%p) from %p \n", pthread_self(), f, s, a);
-  pony_sendv(&f->actor, FUT_MSG_BLOCK, 2, argv);
+  pony_sendv(&f->actor, FUT_MSG_AWAIT, 2, argv);
 
-  pony_actor_unblock(a);
-  t_restart(s, a);
+  t_suspend(s);
 }
 
 void yield(pony_actor_t* a) {
-  stacklet_t *s = t_init();
+  tit_t *s = t_init();
 
   pony_arg_t argv[1];
   argv[0].p = s;
   fprintf(stderr, "[%p]\t%p <--- yield (%p)\n", pthread_self(), a, s);
   pony_sendv(a, FUT_MSG_RESUME, 1, argv);
 
-  t_restart(s, a);
+  t_suspend(s);
 }
 
-bool fulfilled(future *fut) {
-  return fut->payload.fulfilled;
+volatile bool fulfilled(future_t *fut) {
+  return future_actor_get_fulfilled(&fut->actor);
 }
 
-void *getValue(future *fut) {
-  return fut->payload.value;
+volatile void *get_value(future_t *fut) {
+  return future_actor_get_value(&fut->actor);
 }
 
-void *getValueOrBlock(future *fut, pony_actor_t* actor) {
+volatile void *get_value_or_block(future_t *fut, pony_actor_t* actor) {
   if (fulfilled(fut) == false) {
-    block(fut, actor);
+    block(fut, actor, NULL);
   }
-  return fut->payload.value;
+  return get_value(fut);
 }
 
-void fulfil(future *f, void *value) {
-    // XXX: Need to make sure that the entire future payload is written atomically to memory, 
-    // or at least that value is written *before* fulfilled is
-    future_payload atom = { true, value }; 
-    f->payload = atom;
-    fprintf(stderr, "[%p]\t%p <--- fulfil\n", pthread_self(), f);
-    pony_send((pony_actor_t*) f, FUT_MSG_FULFIL);
+void fulfil(future_t *f, volatile void *value) {
+  future_actor_set_value(&f->actor, value);
+  future_actor_set_fulfilled(&f->actor, true);
+  fprintf(stderr, "[%p]\t%p <--- fulfil\n", pthread_self(), f);
+  pony_send(&f->actor, FUT_MSG_FULFIL);
 }
--- src/runtime/future/future_actor.h
+++ src/runtime/future/future_actor.h
@@ -2,7 +2,8 @@
  *  @file future_actor.h
  */
 
-#include "ccontext.h"
+#include <stdbool.h>
+#include "pony/pony.h"
 
 #ifndef __future_actor_h
 #define __future_actor_h
@@ -13,11 +14,17 @@ enum
     FUT_MSG_RESUME,
     FUT_MSG_BLOCK,
     FUT_MSG_CHAIN,
-    FUT_MSG_YIELD,
+    FUT_MSG_AWAIT,
     FUT_MSG_FULFIL
   };
 
+pony_actor_t* future_create();
 pony_msg_t* future_actor_message_type(uint64_t id);
 void future_actor_dispatch(pony_actor_t* this, void* p, uint64_t id, int argc, pony_arg_t* argv);
+volatile void *future_actor_get_value(pony_actor_t* this);
+void future_actor_set_value(pony_actor_t* this, volatile void *value);
+volatile bool future_actor_get_fulfilled(pony_actor_t* this);
+void future_actor_set_fulfilled(pony_actor_t* this, volatile bool fulfilled);
+
 
 #endif
