eac97de4f4514d8addcd5c7001549c4d5ce8f6a9
Elias Castegren <elias.castegren@gmail.com>
2014-07-27 12:02:40 +0200

    Use malloc in the runtime libraries until we've figured out pony_alloc

--- src/runtime/closure/closure.c
+++ src/runtime/closure/closure.c
@@ -1,12 +1,16 @@
 #include "closure.h"
 
 // Control whether pony_alloc is used or not
-#define USE_PONY_ALLOC
+// #define USE_PONY_ALLOC
 
 #ifdef USE_PONY_ALLOC
   #include <pony/pony.h>
+  #define ALLOC(size) pony_alloc(size)
+  #define FREE(ptr)
 #else
   #include <stdlib.h> 
+  #define ALLOC(size) malloc(size)
+  #define FREE(ptr) free(ptr)
 #endif
 
 struct closure{
@@ -15,11 +19,7 @@ struct closure{
 };
 
 closure_t *mk_closure(closure_fun fn, void *env){
-#ifdef USE_PONY_ALLOC
-  closure_t *c = pony_alloc(sizeof(closure_t));
-#else
-  closure_t *c = malloc(sizeof(closure_t));
-#endif
+  closure_t *c = ALLOC(sizeof(closure_t));
   c->call = fn;
   c->env = env;
   return c;
@@ -30,11 +30,8 @@ value_t closure_call(closure_t *closure, value_t args[]){
 }
 
 void closure_free(closure_t *closure){
-#ifdef USE_PONY_ALLOC
-#else
-  free(closure->env); // Leaks copied memory!
-  free(closure);
-#endif
+  FREE(closure->env); // Leaks copied memory!
+  FREE(closure);
 }
 
 value_t ptr_to_val(void *p){
