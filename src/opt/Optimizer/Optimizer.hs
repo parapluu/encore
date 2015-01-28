1e92c460d8ceb1139fe76d3cf0d2c8a9f2b7673c
Daniel S. McCain <dsmccain@acm.org>
2015-01-28 11:48:38 +0100

    Small cleanup of the src/opt Haskell code

--- src/opt/Optimizer/Optimizer.hs
+++ src/opt/Optimizer/Optimizer.hs
@@ -8,9 +8,10 @@ import AST.Util
 import Types
 
 optimizeProgram :: Program -> Program
-optimizeProgram p@(Program{classes, functions}) = p{classes = map optimizeClass classes,
-                                                    functions = map optimizeFunction functions}
-    where
+optimizeProgram p@(Program{classes, functions}) =
+    p{classes = map optimizeClass classes,
+      functions = map optimizeFunction functions}
+  where
       optimizeFunction f@(Function{funbody}) = f{funbody = optimizeExpr funbody}
       optimizeClass c@(Class{methods}) = c{methods = map optimizeMethod methods}
       optimizeMethod m = m{mbody = optimizeExpr (mbody m)}
@@ -26,20 +27,21 @@ optimizerPasses = [constantFolding, constructors]
 -- function.
 constantFolding :: Expr -> Expr
 constantFolding = extend foldConst
-    where
-      foldConst (Binop {emeta = meta, op = PLUS, 
-                        loper = IntLiteral{intLit = m}, 
-                        roper = IntLiteral{intLit = n}}) = 
-          IntLiteral{emeta = meta, intLit = (m + n)}
+  where
+      foldConst (Binop {emeta = meta, op = PLUS,
+                        loper = IntLiteral{intLit = m},
+                        roper = IntLiteral{intLit = n}}) =
+          IntLiteral{emeta = meta, intLit = m + n}
       foldConst e = e
 
 -- Calls to init are necessarily constructor calls and should
 -- therefore be future-less message sends.
 constructors :: Expr -> Expr
 constructors = extend constr
-    where
-      constr e@(MethodCall {name = Name "_init", emeta, target, args}) 
-          | (isActiveRefType . getType) target =
-              MessageSend {name = Name "_init", emeta = emeta, target = target, args = args}
-          | otherwise = e
+  where
+      constr e@(MethodCall {name = Name "_init", emeta, target, args})
+        | (isActiveRefType . getType) target =
+            MessageSend {name = Name "_init", emeta = emeta,
+                         target = target, args = args}
+        | otherwise = e
       constr e = e
