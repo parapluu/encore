1714bf5d5d49cf72810e6035cf96c7ead2ebd585
TheGrandmother <henrik.sommerland@gmail.com>
2015-02-01 23:30:22 +0100

    random tampering

--- experiments/mandelbrot/plot.py
+++ experiments/mandelbrot/plot.py
@@ -5,23 +5,59 @@ Created on 30 jan 2015
 '''
 
 import Image
+import gc
+
+def color_thing(bob):
+    #print bob
+    #bob = 1-bob
+    if (bob < 1/3.0 ):
+        return (int(bob*255*3),0,0)
+    elif (bob > 1/3.0 and bob < 2/3.0):
+        return (255,int((bob-(1/3.0))*255*3),0)
+    else:
+        return (255,255,int((bob-(2/3.0))*255*3))
+    
 
 def lol():
     big_string = open("lol.txt","r").read()
-    splitted = big_string.split(",")
-    width = int(splitted[0])
-    data = map(int, splitted[1:])
+    splitted = big_string.split("\n")
+    max_thing = int(splitted[1])
+    width = int(splitted[2])
+    splitted = splitted[3:-1]
+    #print width
+    #print splitted
+    data = [[0]*width]*width
+    print len(splitted)
+    i  = 0
+    for line in splitted:
+        #print line.split(",")
+        data_line = map(int,line.split(","))
+        data[data_line[0]] = data_line[1:]
+        #print i 
+        i = i+ 1 
     
+    print "done with splittin and such"
+    gc.collect()
+    #print data
     img = Image.new("RGB", (width,width))
+    dudes =  [[(0,0,0)]*width]*width
     pixels = img.load()
     
+    print "building file"
     index = 0
     for i in range(img.size[0]):   
+        #print i
         for j in range(img.size[1]):
-            pixels[i,j] = (data[index]*255, data[index]*255, data[index]*255)
+            if(data[i][j] > max_thing):
+                color = 0
+            else:
+                color = (float(data[i][j])/float(max_thing))
+                
+            pixels[i,j] = color_thing(color)
             index = index + 1  
+    print "done"
     
-    
+    #img.show()
     img.save("mandel.png")
 if __name__ == '__main__':
     lol()
