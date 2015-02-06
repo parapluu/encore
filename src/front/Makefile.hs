f460afca640e52c6a65dee4b0a4a8948669839ab
Elias Castegren <elias.castegren@gmail.com>
2015-02-06 13:35:07 +0100

    Compilation now produces a makefile in the c source directory
    It's intended use is for debugging generated C code by recompiling
    changed `.pony.c` files.

---
+++ src/front/Makefile.hs
@@ -0,0 +1,55 @@
+
+module Makefile where 
+
+import Text.PrettyPrint
+
+import Prelude hiding(all)
+
+($\$) t = ((t <> text "\n") $$)
+tab = text "\t"
+all = text "all"
+clean = text "clean"
+rm args = (text "rm -rf" <+> hsep args)
+phony = text ".PHONY"
+cc args = (text "$(CC)" <+> hsep args)
+flags = text "$(FLAGS)"
+target = text "$(TARGET)"
+inc = text "$(INC)"
+lib = text "$(LIB)"
+deps = text "$(DEPS)"
+dSYM = text ".dSYM"
+i = (text "-I" <+>)
+o = (text "-o" <+>)
+parent = text ".."
+
+generateMakefile :: [String] -> String -> String -> String -> String -> String -> Doc
+generateMakefile classFiles progName compiler ccFlags incPath libs = 
+    decl "CC" [compiler]
+    $$
+    decl "TARGET" [progName]
+    $$
+    decl "INC" [incPath]
+    $$
+    decl "LIB" [libs]
+    $$
+    decl "FLAGS" [ccFlags]
+    $$
+    decl "DEPS" ("shared.c" : classFiles)
+    $\$
+    rule all target
+         empty
+    $\$
+    rule target deps
+         (cc [flags, i inc, i parent, lib, deps, o target])
+    $\$
+    rule clean empty
+         (rm [target, target <> dSYM])
+    $\$
+    rule phony clean
+         empty
+    where
+      decl var rhs = text var <> equals <> hsep (map text rhs)
+      rule target deps cmd
+          | isEmpty cmd = target <> colon <+> deps
+          | otherwise   = target <> colon <+> deps $$
+                          tab <> cmd
