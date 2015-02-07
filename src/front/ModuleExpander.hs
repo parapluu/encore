a04356d22e82c327885d690921b9c389af6bbade
Elias Castegren <elias.castegren@gmail.com>
2015-02-07 17:26:52 +0100

    Cleanup

--- src/front/ModuleExpander.hs
+++ src/front/ModuleExpander.hs
@@ -1,4 +1,3 @@
-
 module ModuleExpander(expandModules) where
 
 import Identifiers
@@ -12,43 +11,39 @@ import Debug.Trace
 
 expandModules :: [FilePath] -> Program -> IO Program
 expandModules importDirs p = expand p
-  where
-    expand p@(Program etl imps funs cls)  = 
- 	  do 
-	  	  impAsts <- mapM importOne imps
-		  rimpAsts <- mapM expand impAsts 
-		  return $ foldr merge p rimpAsts
+    where
+      expand p@(Program etl imps funs cls) =
+          do impAsts <- mapM importOne imps
+             rimpAsts <- mapM expand impAsts
+             return $ foldr merge p rimpAsts
 
-    importOne :: ImportDecl -> IO Program
-    importOne (Import meta (Name target)) = 
-      do
-          let sources = map (\dir -> dir ++ target ++ ".enc") importDirs
-          candidates <- filterM doesFileExist sources
-          sourceName <- case candidates of
-                          [] -> (abort $ "Module \"" ++ target ++ "\" cannot be found in imports! Aborting.")
-                          [src] -> do {  informImport target src; return src }
-                          l@(src:_) -> do { duplicateModuleWarning target l; return src }
-          code <- readFile sourceName
-          ast <- case parseEncoreProgram sourceName code of
-               Right ast  -> return ast
-               Left error -> abort $ show error
-          return ast
+      importOne :: ImportDecl -> IO Program
+      importOne (Import meta (Name target)) =
+          do let sources = map (\dir -> dir ++ target ++ ".enc") importDirs
+             candidates <- filterM doesFileExist sources
+             sourceName <-
+                 case candidates of
+                   [] -> abort $ "Module \"" ++ target ++
+                                 "\" cannot be found in imports! Aborting."
+                   [src] -> do { informImport target src; return src }
+                   l@(src:_) -> do { duplicateModuleWarning target l; return src }
+             code <- readFile sourceName
+             ast <- case parseEncoreProgram sourceName code of
+                      Right ast  -> return ast
+                      Left error -> abort $ show error
+             return ast
 
-merge (Program elt ims funs cls) (Program elt' ims' funs' cls') = Program (emjoin elt elt') (ims ++ ims') (funs ++ funs') (cls ++ cls')
-  where emjoin (EmbedTL meta header body) (EmbedTL meta' header' body') = EmbedTL meta (header ++ header) (body ++ body') 
+merge (Program elt ims funs cls) (Program elt' ims' funs' cls') =
+    Program (emjoin elt elt') (ims ++ ims') (funs ++ funs') (cls ++ cls')
+    where
+      emjoin (EmbedTL meta header body) (EmbedTL meta' header' body') =
+          EmbedTL meta (header ++ header) (body ++ body')
 -- TODO how to join the two meta components?
 
--- 
 informImport name src =
-	putStrLn $ "Importing module " ++ name ++ " from " ++ src
+        putStrLn $ "Importing module " ++ name ++ " from " ++ src
 
 duplicateModuleWarning :: String -> [FilePath] -> IO ()
-duplicateModuleWarning name srcs = 
-	do 
-		putStrLn $ "Warning: Module " ++ name ++ " found in multiple places:"
-		mapM (\src -> putStrLn $ "-- " ++ src) srcs
-		return ()
-		
-
-
-	
+duplicateModuleWarning name srcs =
+    do putStrLn $ "Warning: Module " ++ name ++ " found in multiple places:"
+       mapM_ (\src -> putStrLn $ "-- " ++ src) srcs
