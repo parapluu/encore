5ca75eb70a1b306b031c45c1aba7c7cbcc1f3570
Elias Castegren <elias.castegren@gmail.com>
2015-02-13 07:25:43 +0100

    Added `bench` rule to generated Makefile
    `bench` compiles with `-O3` and without `-ggdb`

--- src/front/Makefile.hs
+++ src/front/Makefile.hs
@@ -3,16 +3,20 @@ module Makefile where
 
 import Text.PrettyPrint
 
+import Data.String.Utils
+
 import Prelude hiding(all)
 
 ($\$) t = ((t <> text "\n") $$)
 tab = text "\t"
 all = text "all"
+bench = text "bench"
 clean = text "clean"
 rm args = (text "rm -rf" <+> hsep args)
 phony = text ".PHONY"
 cc args = (text "$(CC)" <+> hsep args)
 flags = text "$(FLAGS)"
+bench_flags = text "$(BENCH_FLAGS)"
 target = text "$(TARGET)"
 inc = text "$(INC)"
 lib = text "$(LIB)"
@@ -34,6 +38,8 @@ generateMakefile classFiles progName compiler ccFlags incPath libs =
     $$
     decl "FLAGS" [ccFlags]
     $$
+    decl "BENCH_FLAGS" [replace "-ggdb" "-O3" ccFlags]
+    $$
     decl "DEPS" ("shared.c" : classFiles)
     $\$
     rule all target
@@ -42,10 +48,13 @@ generateMakefile classFiles progName compiler ccFlags incPath libs =
     rule target deps
          (cc [flags, i inc, i parent, lib, deps, lib, lib, o target])
     $\$
+    rule bench deps
+         (cc [bench_flags, i inc, i parent, lib, deps, lib, lib, o target])
+    $\$
     rule clean empty
          (rm [target, target <> dSYM])
     $\$
-    rule phony clean
+    rule phony (all <+> bench <+> clean)
          empty
     where
       decl var rhs = text var <> equals <> hsep (map text rhs)
