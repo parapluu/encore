e4ddce04b6efda227c9d1813b085fb6bddc0e7a2
Kiko Fernandez Reyes <kiko.fernandez@it.uu.se>
2016-02-25 15:23:10 +0100

    update array folder

--- src/runtime/array/array.c
+++ src/runtime/array/array.c
@@ -21,45 +21,45 @@ pony_type_t array_type =
 /*   return *ia - *ib; */
 /* } */
 
-int int_cmp(const void *a, const void *b) { 
+int int_cmp(const void *a, const void *b) {
   const intptr_t *ia = (const intptr_t*) a;
   const intptr_t *ib = (const intptr_t*) b;
   return *ia - *ib;
 }
 
 /// Only works on arrays of integers, only callable through embed at the present
-void array_qsort(array_t *a, int64_t start, int64_t end) 
+void array_qsort(array_t *a, int64_t start, int64_t end)
 {
   struct array_t *p = a;
-  qsort(p->elements + start, end, sizeof(encore_arg_t*), int_cmp); 
+  qsort(p->elements + start, end, sizeof(encore_arg_t*), int_cmp);
 }
 
-void array_trace(void *p)
+void array_trace(pony_ctx_t* ctx, void *p)
 {
   struct array_t *array = p;
   if (array->type == ENCORE_ACTIVE) {
     for(size_t i = 0; i < array->size; i++) {
-      pony_traceactor(array->elements[i].p);
+      pony_traceactor(ctx, array->elements[i].p);
     }
   } else if (array->type != ENCORE_PRIMITIVE) {
     for(size_t i = 0; i < array->size; i++) {
-      pony_traceobject(array->elements[i].p, array->type->trace);
+      pony_traceobject(ctx, array->elements[i].p, array->type->trace);
     }
   }
 }
 
-array_t *array_mk(size_t size, pony_type_t *type)
+array_t *array_mk(pony_ctx_t* ctx, size_t size, pony_type_t *type)
 {
-  struct array_t *array = encore_alloc(
+  struct array_t *array = encore_alloc(ctx,
       sizeof(struct array_t) + sizeof(encore_arg_t) * size);
   array->size = size;
   array->type = type;
   return array;
 }
 
-array_t *array_from_array(size_t size, pony_type_t *type, encore_arg_t arr[])
+array_t *array_from_array(pony_ctx_t* ctx, size_t size, pony_type_t *type, encore_arg_t arr[])
 {
-  struct array_t *array = array_mk(size, type);
+  struct array_t *array = array_mk(ctx, size, type);
   for(size_t i = 0; i < size; i++) {
     array_set(array, i, arr[i]);
   }
@@ -92,4 +92,3 @@ inline void array_set(array_t *a, size_t i, encore_arg_t element)
 {
   ((struct array_t *)a)->elements[i] = element;
 }
-
