3ec71f085a3de099081fc52278e0b2a62876147f
Kiko Fernandez Reyes <kiko.fernandez@it.uu.se>
2016-02-25 14:57:21 +0100

    update Closure.hs to use new runtime

--- src/back/CodeGen/Closure.hs
+++ src/back/CodeGen/Closure.hs
@@ -45,7 +45,8 @@ translateClosure closure ctable
              Concat [buildEnvironment envName freeVars,
                      tracefunDecl traceName envName freeVars,
                      Function (Typ "value_t") funName
-                              [(Typ "value_t", Var "_args[]"),
+                              [(Ptr encoreCtxT, encoreCtxVar),
+                               (Typ "value_t", Var "_args[]"),
                                (Ptr void, Var "_env")]
                               (Seq $
                                 extractArguments params ++
@@ -89,11 +90,9 @@ translateClosure closure ctable
           where
             traceMember (name, ty)
                 | Ty.isActiveClassType ty =
-                    Call (Nam "pony_traceactor")
-                         [Cast (Ptr ponyActorT) (getVar name)]
+                    Call ponyTraceActor [Cast (Ptr ponyActorT) (getVar name)]
                 | Ty.isPassiveClassType ty =
-                    Call (Nam "pony_traceobject")
-                         [getVar name, AsLval $ classTraceFnName ty]
+                    Call ponyTraceObject [getVar name, AsLval $ classTraceFnName ty]
                 | otherwise = Comm $ "Not tracing member '" ++ show name ++ "'"
             getVar name =
                 (Deref $ Cast (Ptr $ Struct envName) (Var "p"))
