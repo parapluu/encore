9c0e8685354d1593425e9fca18674a30e83e70e0
Kiko Fernandez Reyes <kiko.fernandez@it.uu.se>
2016-12-16 14:27:01 +0100

    `this` cannot be rebind

--- src/types/Typechecker/Typechecker.hs
+++ src/types/Typechecker/Typechecker.hs
@@ -1015,6 +1015,8 @@ instance Checkable Expr where
     --  E |- name = rhs : void
     doTypecheck assign@(Assign {lhs = lhs@VarAccess{qname}, rhs}) =
         do eLhs <- typecheck lhs
+           when (isThisAccess lhs) $
+                  pushError eLhs ThisReassignmentError
            varIsMutable <- asks $ isMutableLocal qname
            varIsLocal <- asks $ isLocal qname
            unless varIsMutable $
@@ -1028,6 +1030,8 @@ instance Checkable Expr where
         do eLhs <- typecheck lhs
            unless (isLval eLhs) $
                   pushError eLhs NonAssignableLHSError
+           when (isThisAccess lhs) $
+                  pushError eLhs ThisReassignmentError
            mtd <- asks currentMethod
            unless (isNothing mtd || isConstructor (fromJust mtd)) $
                   assertNotValField eLhs
