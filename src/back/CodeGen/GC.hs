3be1ce1b3670f01a6a51860385c35837bf533118
Kiko Fernandez Reyes <kiko.fernandez@it.uu.se>
2017-01-04 11:58:20 +0100

    refactor classdecl and methoddecl
    clean up commit that refactors many repeated methods and puts them into
    their corresponding place, in the `MethodDecl.hs` file. Now, instead of
    writing specific code for each class, the `translate method` has been
    centralised to translate different kinds of methods for different kinds
    of classes (e.g. active classes need to generate methods for futures,
    streams and oneway while passive classes do not). all of this is handle
    under the same `translate method abstraction`.

--- src/back/CodeGen/GC.hs
+++ src/back/CodeGen/GC.hs
@@ -1,9 +1,14 @@
-module CodeGen.GC (gcSend, gcRecv) where
+module CodeGen.GC (gcSend
+                  ,gcRecv
+                  ,ponyGcSendFuture
+                  ,ponyGcSendStream
+                  ,ponyGcSendOneway) where
 
 import CodeGen.CCodeNames
 import CCode.Main
 import qualified AST.AST as A
-import CodeGen.Trace (traceVariable, tracefunCall)
+import qualified Types as Ty
+import CodeGen.Trace (traceVariable, tracefunCall, traceFuture, traceStream)
 
 gcRecv params traceFun = [Embed $ "",
                           Embed $ "// --- GC on receive ----------------------------------------",
@@ -26,3 +31,24 @@ gcSend as expectedTypes traceFuns =
     [Statement $ Call ponySendDoneName [Deref encoreCtxVar],
      Embed $ "// --- GC on sending ----------------------------------------",
      Embed $ ""]
+
+ponyGcSendFuture :: [(Ty.Type, CCode Lval)] -> [CCode Stat]
+ponyGcSendFuture argPairs =
+  [Statement $ Call ponyGcSendName [Deref encoreCtxVar]] ++
+  (map (Statement . uncurry traceVariable) argPairs) ++
+  [Statement . traceFuture $ Var "_fut"] ++
+  [Statement $ Call ponySendDoneName [Deref encoreCtxVar]]
+
+ponyGcSendStream :: [(Ty.Type, CCode Lval)] -> [CCode Stat]
+ponyGcSendStream argPairs =
+  [Statement $ Call ponyGcSendName [Deref encoreCtxVar]] ++
+  (map (Statement . uncurry traceVariable) argPairs) ++
+  [Statement . traceStream $ Var "_stream"] ++
+  [Statement $ Call ponySendDoneName [Deref encoreCtxVar]]
+
+ponyGcSendOneway :: [(Ty.Type, CCode Lval)] -> [CCode Stat]
+ponyGcSendOneway argPairs =
+  [Statement $ Call ponyGcSendName [Deref encoreCtxVar]] ++
+  (map (Statement . uncurry traceVariable) argPairs) ++
+  [Comm "No tracing future for oneway msg"] ++
+  [Statement $ Call ponySendDoneName [Deref encoreCtxVar]]
--- src/back/CodeGen/MethodDecl.hs
+++ src/back/CodeGen/MethodDecl.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE MultiParamTypeClasses, FlexibleInstances #-}
 
 {-| Makes @MethodDecl@ (see "AST") an instance of @Translatable@ (see "CodeGen.Typeclasses") -}
-module CodeGen.MethodDecl where
+module CodeGen.MethodDecl(translate) where
 
 import CodeGen.Typeclasses
 import CodeGen.CCodeNames
@@ -9,8 +9,10 @@ import CodeGen.Expr ()
 import CodeGen.Closure
 import CodeGen.Task
 import CodeGen.ClassTable
+import CodeGen.Type(runtimeType)
 import CodeGen.Function(returnStatement)
 import qualified CodeGen.Context as Ctx
+import qualified CodeGen.GC as Gc
 
 import CCode.Main
 import Data.List (intersect)
@@ -23,11 +25,17 @@ import qualified Types as Ty
 import Control.Monad.State hiding(void)
 import Control.Arrow ((&&&))
 
-instance Translatable A.MethodDecl (A.ClassDecl -> ProgramTable -> CCode Toplevel) where
+instance Translatable A.MethodDecl (A.ClassDecl -> ProgramTable -> [CCode Toplevel]) where
   -- | Translates a method into the corresponding C-function
-  translate mdecl@(A.Method {A.mbody})
-            cdecl@(A.Class {A.cname})
-            table
+  translate mdecl@(A.Method {A.mbody}) cdecl@(A.Class {A.cname}) table =
+    let generalMethod = translateGeneral mdecl cdecl table
+        futureMethod = methodImplWithFuture mdecl cdecl table
+        oneWayMethod = methodImplOneWay mdecl cdecl table
+        streamMethod = methodImplStream mdecl cdecl table
+    in  generalMethod ++ futureMethod ++ oneWayMethod ++ streamMethod
+
+
+translateGeneral mdecl@(A.Method {A.mbody}) cdecl@(A.Class {A.cname}) table
       | A.isStreamMethod mdecl =
     let args = (Ptr (Ptr encoreCtxT), encoreCtxVar) :
                (Ptr . AsType $ classTypeName cname, Var "_this") :
@@ -35,7 +43,7 @@ instance Translatable A.MethodDecl (A.ClassDecl -> ProgramTable -> CCode Topleve
         streamCloseStmt = Statement $
           Call streamClose [encoreCtxVar, streamHandle]
     in
-      Concat $ closures ++ tasks ++
+      return $ Concat $ closures ++ tasks ++
                [Function void name args
                  (Seq [extractTypeVars, bodys, streamCloseStmt])]
       | otherwise =
@@ -46,7 +54,7 @@ instance Translatable A.MethodDecl (A.ClassDecl -> ProgramTable -> CCode Topleve
                then [(array, Var "_argv")]
                else zip argTypes argNames
     in
-      Concat $ closures ++ tasks ++
+      return $ Concat $ closures ++ tasks ++
                [Function returnType name args
                  (Seq [extractTypeVars, bodys, returnStatement mType bodyn])]
     where
@@ -75,3 +83,144 @@ instance Translatable A.MethodDecl (A.ClassDecl -> ProgramTable -> CCode Topleve
                      (reverse (Util.filter A.isClosure mbody))
       tasks = map (\tas -> translateTask tas table) $
                   reverse $ Util.filter A.isTask mbody
+
+
+methodImplWithFuture m cdecl@(A.Class {A.cname}) _
+  | A.isActive cdecl ||
+    A.isShared cdecl =
+    let retType = future
+        fName = methodImplFutureName cname mName
+        args = (Ptr (Ptr encoreCtxT), encoreCtxVar) : this : zip argTypes argNames
+        fBody = Seq $
+           runtimeTypeAssignment mType :
+           assignFut :
+           Gc.ponyGcSendFuture argPairs ++
+           msg ++ [retStmt]
+    in [Function retType fName args fBody]
+  | otherwise = []
+  where
+    thisName = "_this"
+    mName = A.methodName m
+    mParams = A.methodParams m
+    mType = A.methodType m
+    runtimeTypeAssignment mtype
+      | Ty.isTypeVar mtype =
+          Assign (Decl (Ptr ponyTypeT, AsLval $ typeVarRefName mtype))
+                 (Arrow (Nam "_this") (typeVarRefName mType))
+      | otherwise = Skip
+    argNames = map (AsLval . argName . A.pname) mParams
+    argTypes = map (translate . A.ptype) mParams
+    this = (Ptr . AsType $ classTypeName cname, Var thisName)
+    futVar = Var "_fut"
+    declFut = Decl (future, futVar)
+    futureMk mtype = Call futureMkFn [AsExpr encoreCtxVar,
+                                      runtimeType mtype]
+    assignFut = Assign declFut $ futureMk mType
+    argPairs = zip (map A.ptype mParams) argNames
+    msg = sendFutMsg cname mName $ map (argName . A.pname) mParams
+    retStmt = Return futVar
+
+methodImplOneWay m cdecl@(A.Class {A.cname}) _
+  | A.isActive cdecl ||
+    A.isShared cdecl =
+      let retType = void
+          fName = methodImplOneWayName cname mName
+          args = (Ptr (Ptr encoreCtxT), encoreCtxVar): this :
+                   zip argTypes argNames
+          fBody = Seq $ Gc.ponyGcSendOneway argPairs ++ msg
+      in return $ Function retType fName args fBody
+  | otherwise = []
+  where
+    thisName = "_this"
+    mName = A.methodName m
+    mParams = A.methodParams m
+    argNames = map (AsLval . argName . A.pname) mParams
+    argTypes = map (translate . A.ptype) mParams
+    this = (Ptr . AsType $ classTypeName cname, Var thisName)
+
+    argPairs = zip (map A.ptype mParams) argNames
+    msg = sendOneWayMsg cname mName $ map (argName . A.pname) mParams
+
+methodImplStream m cdecl@(A.Class {A.cname}) _
+  | A.isStreamMethod m =
+    let retType = stream
+        fName = methodImplStreamName cname mName
+        args = (Ptr (Ptr encoreCtxT), encoreCtxVar) : this : zip argTypes argNames
+        fBody = Seq $ [assignFut] ++ Gc.ponyGcSendStream argPairs ++
+                      msg ++ [retStmt]
+    in return $ Function retType fName args fBody
+  | otherwise = []
+  where
+    thisName = "_this"
+    mName = A.methodName m
+    mParams = A.methodParams m
+    mType = A.methodType m
+    argNames = map (AsLval . argName . A.pname) mParams
+    argTypes = map (translate . A.ptype) mParams
+    this = (Ptr . AsType $ classTypeName cname, Var thisName)
+    retVar = Var "_stream"
+    declVar = Decl (stream, retVar)
+    streamMk mtype = Call streamMkFn [encoreCtxVar]
+    assignFut = Assign declVar $ streamMk mType
+    argPairs = zip (map A.ptype mParams) argNames
+    msg = sendStreamMsg cname mName $ map (argName . A.pname) mParams
+    retStmt = Return retVar
+
+sendFutMsg :: Ty.Type -> ID.Name -> [CCode Name] -> [CCode Stat]
+sendFutMsg cname mname args =
+  let
+    msgId = futMsgId cname mname
+    msgTypeName = futMsgTypeName cname mname
+    fields = [Nam $ "f" ++ show i | i <- [1..length args]]
+    argPairs = zip fields args ++ [(Nam "_fut", Nam "_fut")]
+  in
+    sendMsg cname mname msgId msgTypeName argPairs
+
+sendOneWayMsg :: Ty.Type -> ID.Name -> [CCode Name] -> [CCode Stat]
+sendOneWayMsg cname mname args =
+  let
+    msgId = oneWayMsgId cname mname
+    msgTypeName = oneWayMsgTypeName cname mname
+    fields = [Nam $ "f" ++ show i | i <- [1..length args]]
+    argPairs = zip fields args
+  in
+    sendMsg cname mname msgId msgTypeName argPairs
+
+sendStreamMsg :: Ty.Type -> ID.Name -> [CCode Name] -> [CCode Stat]
+sendStreamMsg cname mname args =
+  let
+    msgId = futMsgId cname mname
+    msgTypeName = futMsgTypeName cname mname
+    fields = [Nam $ "f" ++ show i | i <- [1..length args]]
+    argPairs = zip fields args ++ [(Nam "_fut", Nam "_stream")]
+  in
+    sendMsg cname mname msgId msgTypeName argPairs
+
+sendMsg :: Ty.Type -> ID.Name -> CCode Name -> CCode Name
+  -> [(CCode Name, CCode Name)]
+  -> [CCode Stat]
+sendMsg cname mname msgId msgTypeName argPairs = [
+  assignMsg
+  , initMsg
+  , sendMsg
+  ]
+  where
+    thisName = "_this"
+    msgType = AsType msgTypeName
+    msgTypePtr = Ptr msgType
+    msgName = "msg"
+    declMsg = Decl (msgTypePtr, Var msgName)
+    msgIdExpr = AsExpr. AsLval $ msgId
+    msgSizeIndex = Call poolIndexName [Sizeof msgType]
+    allocMsg = Call ponyAllocMsgName [msgSizeIndex, msgIdExpr]
+    castAllocMsg = Cast msgTypePtr allocMsg
+    assignMsg = Assign declMsg castAllocMsg
+
+    assignMsgFields =
+      [Assign (Var msgName `Arrow` f) rhs | (f,rhs) <- argPairs]
+    initMsg = Seq assignMsgFields
+
+    target = Cast (Ptr ponyActorT) $ Var thisName
+    msgArg = Cast (Ptr ponyMsgT) $ Var msgName
+    sendMsg = Statement $
+      Call ponySendvName [AsExpr $ Deref encoreCtxVar, target, msgArg]
