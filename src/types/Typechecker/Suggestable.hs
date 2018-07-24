cbbea76f60be49f4c642b502b47d94b60d01876d
Thizizmyname <Alexis.remmers@gmail.com>
2018-07-24 15:37:04 +0200

    Added TypeMismatchError and explanation

--- src/types/Typechecker/Suggestable.hs
+++ src/types/Typechecker/Suggestable.hs
@@ -25,8 +25,8 @@ pipe = char '|'
 highlightPretty :: String -> Doc TCStyle
 highlightPretty s = highlight $ text s
 
-makeNotation :: Doc TCStyle
-makeNotation = logistic (pipe $+$ equals) <+> desc (text "note:")
+codeViewerNote :: Doc TCStyle
+codeViewerNote = logistic (pipe $+$ equals) <+> desc (text "note:")
 
 -- How to determine if to use a smallSuggest or longSuggest:
 -- If a problem justifies it, you could use both, 
@@ -44,20 +44,28 @@ instance Suggestable TCError where
     smallSuggest (TCError (NonAssignableLHSError) _) = highlightPretty "Can only be used on var or fields"
     smallSuggest (TCError (MethodNotFoundError name ty) env)
         | isMethodNameAFunction name ty env = highlightPretty $ printf "Did you mean function `%s`?" (show name)
+    smallSuggest (TCError (TypeMismatchError actual expected) _) =
+        highlightPretty $ printf "expected %s" (show expected)
     smallSuggest _ = empty
 
+    longSuggest (TCError (TypeMismatchError actual expected) _) =
+        let
+            expect = text "expected type" <+> desc (text $ show expected)
+            found  = text "   found type" <+> desc (text $ show actual)
+        in
+            codeViewerNote <+> vcat [expect, found]
     longSuggest (TCError (TypeWithCapabilityMismatchError actual cap expected) _) =
         let
             expect = text "expected type" <+> desc (text $ show expected)
             found  = text "   found type" <+> desc (text $ show actual)
         in
-            makeNotation <+> vcat [expect, found]
+            codeViewerNote <+> vcat [expect, found]
     longSuggest (TCError (WrongNumberOfMethodArgumentsError name targetType _ _) env) = 
         let 
             header = snd . fromJust $ findMethodWithEnvironment name targetType env
             types = hparams header
         in
-            makeNotation <+> hang ("Method" <+> quotes (text $ show name) <+> "is declared:") 0
+            codeViewerNote <+> hang ("Method" <+> quotes (text $ show name) <+> "is declared:") 0
                 (desc (ppFunctionHeader header))
     
     longSuggest _ = empty
