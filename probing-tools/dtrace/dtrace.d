ffe88ac2155d0730e6475116abfe30950505687b
Ulf Sigvardsson <ulf_sigvardsson@hotmail.com>
2019-02-25 22:30:24 +0100

    prettified the output

--- probing-tools/dtrace/dtrace.d
+++ probing-tools/dtrace/dtrace.d
@@ -1,27 +1,17 @@
 #pragma D option quiet
 
-BEGIN {
-<<<<<<< HEAD
+BEGIN
+{
 	total_steals = 0;
 	total_failed_steals = 0;
 	total_steal_attempts = 0;
 	total_schedulings = 0;
 }
 
-
-=======
-	printf("Dtrace started!");
-}
-
-END {
-	printf("Dtrace finished!");
-}
->>>>>>> 8a72325950f12b752f7f408fb9439a5cc5e393a6
-
 pony$target:::actor-alloc { }
 pony$target:::actor-msg-send { }
 pony$target:::actor-msg-run { }
-<<<<<<< HEAD
+
 
 // arg[0] is scheduler, arg[1] is actor
 pony$target:::actor-scheduled
@@ -30,9 +20,6 @@ pony$target:::actor-scheduled
 	@schedulers_for_actor[args[0], args[1]] = count();
 }
 
-=======
-pony$target:::actor-scheduled { }
->>>>>>> 8a72325950f12b752f7f408fb9439a5cc5e393a6
 pony$target:::actor-descheduled { }
 pony$target:::cpu-nanosleep { }
 pony$target:::gc-end {}
@@ -46,24 +33,34 @@ pony$target:::heap-alloc {}
 pony$target:::rt-init {}
 pony$target:::rt-start {}
 pony$target:::rt-end {}
-<<<<<<< HEAD
 
+	/**
+   * Fired when a scheduler succesfully steals a job
+   * @param scheduler is the scheduler that stole the job
+   * @param victim is the victim that the scheduler stole from
+   * @param actor is actor that was stolen from the victim
+   */
 pony$target:::work-steal-successful
 {
 	total_steals++;
 	total_steal_attempts++;
   @steal_success_count[args[0]] = count();
+	@successful_steal_from_scheduler[args[0], args[1]] = count();
+	@stolen_actor[args[2]] = count();
 }
+
+/**
+ * Fired when a scheduler fails to steal a job
+ * @param scheduler is the scheduler that attempted theft
+ * @param victim is the victim that the scheduler attempted to steal from
+ */
 pony$target:::work-steal-failure
 {
 	total_failed_steals++;
 	total_steal_attempts++;
   @steal_fail_count[args[0]] = count();
+	@failed_steal_from_scheduler[args[0], args[1]] = count();
 }
-=======
-pony$target:::work-steal-successful {}
-pony$target:::work-steal-failure {}
->>>>>>> 8a72325950f12b752f7f408fb9439a5cc5e393a6
 
 encore$target:::closure-create {}
 encore$target:::future-block {}
@@ -85,28 +82,43 @@ encore$target:::function-exit {}
 // encore$target:::closure-call {}
 // encore$target:::closure-entry {}
 // encore$target:::closure-exit {}
-<<<<<<< HEAD
+
 
 END {
-	printf("Total steal attempts:\t\t %d\n", total_steal_attempts);
-	printf("Total successful steals:\t %d\n", total_steals);
-	printf("Total failed steal attempts:\t %d\n", total_failed_steals);
-	printf("Total schedulings: \t\t%d\n", total_schedulings);
+	printf("==========================================\n\t\tSTEALS\n==========================================\n");
+	printf("\nTOTAL\n");
+	printf("Attempted\tSuccessful\tFailed\n");
+	printf("%d\t\t%d\t\t%d\n", total_steal_attempts, total_steals, total_failed_steals);
 
 	printf("\n");
-	printf("Actors scheduled by Schedulers\n");
-	printf("Scheduler ID\tActor ID\tCount\n");
-	printa("%d\t%d%@8u\n", @schedulers_for_actor);
-
-	printf("\n");
-	printf("Successive steals\n");
+	printf("SUCCESSIVE STEALS\n");
 	printf("Scheduler ID\tCount\n");
 	printa("%d%@8u\n", @steal_success_count);
 
 	printf("\n");
-	printf("Failed steals\n");
+	printf("FAILED STEALS\n");
 	printf("Scheduler ID\tCount\n");
 	printa("%d%@8u\n", @steal_fail_count);
+
+	printf("\nSTEALS BETWEEN SCHEDULERS\n");
+		printf("Stolen by\tStolen from\tCount\n");
+	printa("%d\t%d%@8u\n", @successful_steal_from_scheduler);
+
+	printf("\nFAILS BETWEEN SCHEDULERS\n");
+	printf("Attempted by\tTarget\t\tCount\n");
+	printa("%d\t%d%@8u\n", @failed_steal_from_scheduler);
+
+	printf("\nSTOLEN ACTORS\n");
+	printf("Actor ID\tTimes stolen\n");
+	printa("%d%@8u\n", @stolen_actor);
+
+	printf("\n==========================================\n\t\tSCHEDULING\n==========================================\n");
+	printf("\nTOTAL SCHEDULINGS: %d\n", total_schedulings);
+
+	printf("\n");
+	printf("DISTRIBUTION\n");
+	printf("Scheduler ID\tActor ID\tCount\n");
+	printa("%d\t%d%@8u\n", @schedulers_for_actor);
+
+
 }
-=======
->>>>>>> 8a72325950f12b752f7f408fb9439a5cc5e393a6
