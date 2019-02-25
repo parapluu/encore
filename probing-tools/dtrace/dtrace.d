84ef51d56b4b33ddc6096c7300c0f1a04c96fed5
Ulf Sigvardsson <ulf_sigvardsson@hotmail.com>
2019-02-25 21:47:45 +0100

    actor steal and scheduling count

---
+++ probing-tools/dtrace/dtrace.d
@@ -0,0 +1,91 @@
+#pragma D option quiet
+
+BEGIN {
+	total_steals = 0;
+	total_failed_steals = 0;
+	total_steal_attempts = 0;
+	total_schedulings = 0;
+}
+
+
+
+pony$target:::actor-alloc { }
+pony$target:::actor-msg-send { }
+pony$target:::actor-msg-run { }
+
+// arg[0] is scheduler, arg[1] is actor
+pony$target:::actor-scheduled
+{
+	total_schedulings++;
+	@schedulers_for_actor[args[0], args[1]] = count();
+}
+
+pony$target:::actor-descheduled { }
+pony$target:::cpu-nanosleep { }
+pony$target:::gc-end {}
+pony$target:::gc-send-end {}
+pony$target:::gc-send-start {}
+pony$target:::gc-recv-end {}
+pony$target:::gc-recv-start {}
+pony$target:::gc-start {}
+pony$target:::gc-threshold {}
+pony$target:::heap-alloc {}
+pony$target:::rt-init {}
+pony$target:::rt-start {}
+pony$target:::rt-end {}
+
+pony$target:::work-steal-successful
+{
+	total_steals++;
+	total_steal_attempts++;
+  @steal_success_count[args[0]] = count();
+}
+pony$target:::work-steal-failure
+{
+	total_failed_steals++;
+	total_steal_attempts++;
+  @steal_fail_count[args[0]] = count();
+}
+
+encore$target:::closure-create {}
+encore$target:::future-block {}
+encore$target:::future-chaining {}
+encore$target:::future-create {}
+encore$target:::future-destroy {}
+encore$target:::future-fulfil-start {}
+encore$target:::future-fulfil-end {}
+encore$target:::future-get {}
+encore$target:::future-unblock {}
+encore$target:::field-access {}
+encore$target:::field-write {}
+// encore$target:::method-call {}
+encore$target:::method-entry {}
+encore$target:::method-exit {}
+encore$target:::function-call {}
+encore$target:::function-entry {}
+encore$target:::function-exit {}
+// encore$target:::closure-call {}
+// encore$target:::closure-entry {}
+// encore$target:::closure-exit {}
+
+END {
+	printf("Total steal attempts:\t\t %d\n", total_steal_attempts);
+	printf("Total successful steals:\t %d\n", total_steals);
+	printf("Total failed steal attempts:\t %d\n", total_failed_steals);
+	printf("Total schedulings: \t\t%d\n", total_schedulings);
+
+	printf("\n");
+	printf("Actors scheduled by Schedulers\n");
+	printf("Scheduler ID\tActor ID\tCount\n");
+	printa("%d\t%d%@8u\n", @schedulers_for_actor);
+
+	printf("\n");
+	printf("Successive steals\n");
+	printf("Scheduler ID\tCount\n");
+	printa("%d%@8u\n", @steal_success_count);
+
+	printf("\n");
+	printf("Failed steals\n");
+	printf("Scheduler ID\tCount\n");
+	printa("%d%@8u\n", @steal_fail_count);
+}
